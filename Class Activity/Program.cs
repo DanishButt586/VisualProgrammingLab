de9df902907b85c718e23a2f18c8ff743eed0cb2
DanishButt586 <233606@students.au.edu.pk>
2024-10-22 20:01:45 +0500

    Add files via upload

---
+++ Class Activity/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        //--> Activity 1
+
+        double a = 98, b = 0;
+        double result = 0;
+        try
+        {
+            result = SafeDivision(a, b);
+            Console.WriteLine($"{a} divided by {b} = {result}");
+
+
+        }
+        catch (DivideByZeroException e)
+        {
+            { Console.WriteLine("Attempted divide by zero."); }
+        }
+
+
+        //-->Activity 2
+
+        TestCatch2();
+
+
+
+
+    }
+    //-->Activity 1 (Function)
+    static double SafeDivision(double x, double y)
+    {
+        if (y == 0)
+            throw new System.DivideByZeroException();
+        return x / y;
+    }
+
+
+    //-->Activity 2 (Function)
+    static void TestCatch2()
+    {
+        System.IO.StreamWriter sw = null;
+        try
+        {
+            sw = new System.IO.StreamWriter(@"C:\test\test.txt");
+            sw.WriteLine("Hello");
+        }
+        catch (System.IO.FileNotFoundException ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        catch (System.IO.IOException ex)
+        {
+            System.Console.WriteLine(ex.ToString());
+        }
+        finally
+        {
+            sw.Close();
+        }
+        System.Console.WriteLine("Done");
+    }
+}
+
